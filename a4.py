8b8b6f0548d8ef1b9153d2557931f62528bd0b46
Eddie <eyortega1@cps.edu>
2024-10-30 11:00:03 -0500

    This is what I have so far

--- a4.py
+++ a4.py
@@ -9,6 +9,49 @@ class TTTBoard:
         board - a list of '*'s, 'X's & 'O's. 'X's represent moves by player 'X', 'O's
             represent moves by player 'O' and '*'s are spots no one has yet played on
     """
+    def __init__(self) -> list:
+
+        self.board = ['*'] * 9
+
+    def __str__(self):
+        return (
+            f"{self.board[0]} | {self.board[1]} | {self.board[2]}\n"
+            "---------\n"
+            f"{self.board[3]} | {self.board[4]} | {self.board[5]} \n"
+            "---------\n"
+            f"{self.board[6]} | {self.board[7]} | {self.board[8]} \n"
+        )
+
+    def make_move(self, player, pos):
+        if player not in {'X', 'O'}:
+            raise ValueError("Player must be 'x' or 'O'.")
+        if pos < 0 or pos >= 9:
+            return False
+        if self.board[pos] == '*':
+            self.board[pos] = player
+            return True
+        return False    
+        
+    def has_won(self, player):
+        winning_combinations = [
+            (0,1,2),
+            (3,4,5),
+            (6,7,8),
+            (0,3,6),
+            (1,4,7),
+            (2,5,8),
+            (0,4,8),
+            (2,4,6),
+        ]
+        return any(all(self.board[i] == player for i in combo) for combo in winning_combinations)
+
+    def game_over(self): 
+        return self.has_won('X') or self.has_won('O') or '*' not in self.board
+    
+    def clear(self): 
+        self.board = ['*'] * 9
+
+
 
     pass
 
@@ -89,4 +132,4 @@ if __name__ == "__main__":
     print("All tests passed!")
 
     # uncomment to play!
-    # play_tic_tac_toe()
+    play_tic_tac_toe()
